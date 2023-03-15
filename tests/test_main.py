94a8a4ae7c9a2ee38011ffb695f1c2997296c375
piroinno <peter.irojah@piroinno.com>
2023-03-15 01:24:34 +0000

    removed non used list

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,15 +5,19 @@ from sqlalchemy.orm import sessionmaker
 import pytest
 from stock.data.model.database import SessionLocal, Base, engine
 
+
 def init_test_db():
     Base.metadata.create_all(bind=engine)
+
 
 def drop_test_db():
     Base.metadata.drop_all(bind=engine)
 
+
 def recreate_test_db():
     drop_test_db()
     init_test_db()
+
 
 @pytest.fixture(scope="session")
 def db():
@@ -29,6 +33,7 @@ def db():
     finally:
         db.close()
 
+
 def add_timezone(db: SessionLocal):
     # Add test data
     crud.set_timezone(db, timezone=models.TimezoneModel(
@@ -38,6 +43,7 @@ def add_timezone(db: SessionLocal):
     crud.set_timezone(db, timezone=models.TimezoneModel(
         name="Greenwich Mean Time", abbr="GMT", dst="BST"
     ))
+
 
 def add_city(db: SessionLocal):
     # Add test data
@@ -49,6 +55,7 @@ def add_city(db: SessionLocal):
         name="London", country_id=2
     ))
 
+
 def add_country(db: SessionLocal):
     # Add test data
     crud.set_country(db, country=models.CountryModel(
@@ -59,34 +66,33 @@ def add_country(db: SessionLocal):
         name="United Kingdom", code="UK"
     ))
 
+
 def add_ticker(db: SessionLocal):
     # Add test data
     crud.set_ticker(db, ticker=models.TickerModel(
         ticker="AAPL", name="Apple Inc.", exchange_id=1
     ))
 
+
 def add_exchanges(db: SessionLocal):
     # Add test data
-    crud.set_exchange(db, exchange=[
-        models.ExchangeModel(
-            name="National Association of Securities Dealers Automated Quotations", country_id=1, city_id=1, timezone_id=1,
-            acronym="NASDAQ", mic="XNAS"
-        )
-    ])
+    crud.set_exchange(db, exchange=models.ExchangeModel(
+        name="National Association of Securities Dealers Automated Quotations", country_id=1, city_id=1, timezone_id=1,
+        acronym="NASDAQ", mic="XNAS"
+    )
+    )
 
-    crud.set_exchange_list(db, exchange=[
-        models.ExchangeModel(
-            name="New York Stock Exchange", country_id=1, city_id=1, timezone_id=1,
-            acronym="NYSE", mic="XNYS"
-        )
-    ])
+    crud.set_exchange(db, exchange=models.ExchangeModel(
+        name="New York Stock Exchange", country_id=1, city_id=1, timezone_id=1,
+        acronym="NYSE", mic="XNYS"
+    )
+    )
 
-    crud.set_exchange_list(db, exchange=[
-        models.ExchangeModel(
-            name="London Stock Exchange", country_id=2, city_id=2, timezone_id=2,
-            acronym="LSE", mic="XLON"
-        )
-    ])
+    crud.set_exchange(db, exchange=models.ExchangeModel(
+        name="London Stock Exchange", country_id=2, city_id=2, timezone_id=2,
+        acronym="LSE", mic="XLON"
+    )
+    )
 
 
 def test_get_ticker(db: SessionLocal):
@@ -94,19 +100,23 @@ def test_get_ticker(db: SessionLocal):
     assert ticker.id == 1
     assert ticker.ticker == "AAPL"
 
+
 def test_get_tickers(db: SessionLocal):
     tickers = crud.get_tickers(db)
     assert len(tickers) == 1
+
 
 def test_get_ticker_by_name(db: SessionLocal):
     ticker = crud.get_ticker_by_name(db, ticker="AAPL")
     assert ticker.id == 1
     assert ticker.ticker == "AAPL"
 
+
 def test_get_exchange(db: SessionLocal):
     exchange = crud.get_exchange(db, exchange_id=1)
     assert exchange.id == 1
     assert exchange.exchange == "NASDAQ"
+
 
 def test_get_exchanges(db: SessionLocal):
     exchanges = crud.get_exchanges(db)
